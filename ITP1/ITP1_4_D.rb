6fde136ba40418138e1ae7956df5b907be19ba98
defabc <defabc2231@gmail.com>
2016-08-12 20:19:21 +0900

    Fixed  ITP1_4_D

--- ITP1/ITP1_4_D.rb
+++ ITP1/ITP1_4_D.rb
@@ -1,13 +1,12 @@
 n = gets.to_i
 
 begin
-  num_array = gets.split(" ").map(&:to_i)
-end while (num_array.length != n)
+  num_ary = gets.split(" ").map(&:to_i)
+end while (num_ary.length != n)
 
-sorted_num_ary = num_array.sort
 
-min = sorted_num_ary.min
-max = sorted_num_ary.max
-sum = num_array.inject(0) {|start, a| start + a }
+min = num_ary.min
+max = num_ary.max
+sum = num_ary.inject(0) {|p, q| p + q }
 
 puts "#{min} #{max} #{sum}"
