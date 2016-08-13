b38c03d6d33feaa580b05a8e103bf9d9607d3755
defabc <defabc2231@gmail.com>
2016-08-13 23:35:01 +0900

    Added another function

--- ITP1/ITP1_5_A.rb
+++ ITP1/ITP1_5_A.rb
@@ -3,13 +3,23 @@ def print_rectangle(h, w)
     w.times do |j|
       print "#"
     end
-    print "\n" if i < h - 1
+    print "\n"
+  end
+end
+
+def print_rectangle2(h, w)
+  for i in 1..h
+    for j in 1..w
+      print "#"
+    end
+    print "\n"
   end
 end
 
 while true do
   a, b = gets.split(" ").map {|s| s.to_i}
   break if a == 0 && b  == 0
-  print_rectangle(a, b)
-  print "\n\n"
+  # print_rectangle(a, b)
+  print_rectangle2(a, b)
+  print "\n"
 end
