c80e0f1704b5df923aa9443443aa21a2c37c96c7
defabc <defabc2231@gmail.com>
2016-11-01 01:06:25 +0900

    edit ITP1_7_A.rb

--- ITP1/ITP1_7_A.rb
+++ ITP1/ITP1_7_A.rb
@@ -1,36 +1,17 @@
-class Student
-  attr_reader :total_score, :re, :mid_score, :end_score
-
-  def initialize(mid_score, end_score, re)
-    @mid_score   = mid_score
-    @end_score   = end_score
-    @total_score = mid_score + end_score
-    @re          = re
-  end
-
-  def grading
-    if mid_score == -1 or end_score == -1
-      return 'F'
-    elsif total_score >= 80
-      return 'A'
-    elsif total_score < 80 && total_score >= 65
-      return 'B'
-    elsif total_score < 65 && total_score >= 50
-      return 'C'
-    elsif total_score < 50 && total_score >= 30
-      if re >= 50
-        return 'C'
-      else
-        return 'D'
-      end
-    else
-      return 'F'
-    end
-  end
-end
-
-while true
-  a, b, c = gets.split(" ").map(&:to_i)
-  break if a == -1 && b == -1 && c == -1
-  puts Student.new(a, b, c).grading
+while (a, b, c = gets.split(" ").map(&:to_i)) != [-1, -1, -1]
+  total_score = a + b
+  grading = if a == -1 or b == -1
+              'F'
+            elsif total_score >= 80
+              'A'
+            elsif total_score < 80 && total_score >= 65
+              'B'
+            elsif total_score < 65 && total_score >= 50
+              'C'
+            elsif total_score < 50 && total_score >= 30
+              c >= 50 ? 'C' : 'D'
+            else
+              'F'
+            end
+  puts grading
 end
