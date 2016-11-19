5edde7784abd92ad34a50d7003403597df4eb1d9
defabc <defabc2231@gmail.com>
2016-11-19 20:14:06 +0900

    edit ITP1_8_D.rb

--- ITP1/ITP1_8_D.rb
+++ ITP1/ITP1_8_D.rb
@@ -2,7 +2,8 @@ input = gets.chomp
 input_str = input * 2
 finding_str = gets.chomp
 
-if input_str =~ /#{finding_str}/
+# if input_str =~ /#{finding_str}/
+if input_str.include?(finding_str) 
   puts "Yes"
 else
   puts "No"
